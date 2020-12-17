dae5592cb24b3598142907005440f8dde89dd1c5
Logan Thirion <thirionlogan@gmail.com>
2020-12-17 14:58:39 -0600

    #3 create express server

--- server/app.js
+++ server/app.js
@@ -6,8 +6,32 @@ const app = express();
 
 app.use(bodyParser.json());
 
-app.get('/', (req, res) => {
-  res.status(200).send('Hello World!');
+app.get('/wishlist', (req, res) => {
+  res.status(200).send({
+    message: 'Hello!',
+  });
+});
+
+app.get('/wishlist/:id', (req, res) => {
+  res.status(200).send({
+    message: `Hello! Your request id is:${req.params.id}`,
+  });
+});
+
+app.post('/wishlist', (req, res) => {
+  res.status(201).send({ message: 'Your wishlist has been created!' });
+});
+
+app.delete('/wishlist/:id', (req, res) => {
+  res
+    .status(200)
+    .send({ message: `request ${req.params.id} has been deleted` });
+});
+
+app.put('/wishlist/:id', (req, res) => {
+  res
+    .status(200)
+    .send({ message: `request ${req.params.id} has been updated` });
 });
 
 module.exports = app;
