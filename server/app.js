dcf3da41ba382dceeb2c2fc707f68264da697150
brentogunmakin <brent.ogunmakin@us.af.mil>
2020-12-17 13:14:45 -0700

    added image and search box

---
+++ server/app.js
@@ -0,0 +1,18 @@
+import { Component } from "react";
+import PageHeader from "../src/components/PageHeader/PageHeader";
+import "./Pageheader.css";
+
+
+class App extends Component {
+
+
+    render() {
+        return(
+           <>
+           </>
+        )
+    }
+
+}
+
+export default App;
---
+++ src/components/PageHeader/PageHeader.js
@@ -0,0 +1,20 @@
+import { Component } from "react";
+import "./Pageheader.css";
+
+
+class PageHeader extends Component {
+
+
+    render() {
+        return(
+            <>
+            <img src='./amazonia_logo.svg'></img>
+
+            <input type='text'></input>
+            </>
+        )
+    }
+
+}
+
+export default PageHeader;
