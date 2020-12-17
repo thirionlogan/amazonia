ef0c0cf5df43ee8ccac435670a315b068e9d4b05
brentogunmakin <brent.ogunmakin@us.af.mil>
2020-12-17 13:55:40 -0700

    Styled Header

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,25 +1,20 @@
-import logo from '../../logo.svg';
-import './App.css';
+import { Component } from "react";
+import PageHeader from "../PageHeader/PageHeader";
 
-function App() {
-  return (
-    <div className='App'>
-      <header className='App-header'>
-        <img src={logo} className='App-logo' alt='logo' />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className='App-link'
-          href='https://reactjs.org'
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import './App.css'
+
+
+class App extends Component {
+
+
+    render() {
+        return(
+           <>
+            <PageHeader />
+           </>
+        )
+    }
+
 }
 
 export default App;
--- src/components/PageHeader/PageHeader.js
+++ src/components/PageHeader/PageHeader.js
@@ -1,17 +1,45 @@
 import { Component } from "react";
-import "./Pageheader.css";
+//import "./Pageheader.css";
+import Logo from './logo_new.svg';
+import styled from 'styled-components';
 
+
+const Header = styled.header `
+    background-color: #474747;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    width: 100%;
+    padding-bottom: 10px;
+    padding-top: 10px;
+
+
+    `;
+
+
+const Image = styled.img `
+    padding-left: 50px;
+    height: 50px;
+    width: 100px
+    `;
+
+const SearchBar = styled.input `
+    height: 30px;
+    border-radius: 10px;
+    border: 1px solid white;
+    margin-right: 15px
+    `;
 
 class PageHeader extends Component {
-
+    
 
     render() {
         return(
-            <>
-            <img src='./amazonia_logo.svg'></img>
+            <Header>
+            <Image src={Logo} alt='logo'></Image>
 
-            <input type='text'></input>
-            </>
+            <SearchBar type='text' placeholder='Search'></SearchBar>
+            </Header>
         )
     }
 
