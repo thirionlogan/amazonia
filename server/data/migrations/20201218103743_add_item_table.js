7b33300bcea754f2f1e180e55b3b33768d27262b
Logan Thirion <thirionlogan@gmail.com>
2020-12-18 11:45:02 -0600

    fix orphaned items

--- server/data/migrations/20201218103743_add_item_table.js
+++ server/data/migrations/20201218103743_add_item_table.js
@@ -2,7 +2,11 @@ exports.up = (knex) => {
   return knex.schema.createTable('item', (table) => {
     table.increments('id').primary();
     table.integer('wishlist_id');
-    table.foreign('wishlist_id').references('id').inTable('wishlist');
+    table
+      .foreign('wishlist_id')
+      .references('id')
+      .inTable('wishlist')
+      .onDelete('CASCADE');
     table.string('name');
   });
 };
