a81a45872b3c29f2c65143dedb9d29b0c814bca4
Logan Thirion <thirionlogan@gmail.com>
2020-12-18 17:50:32 -0600

    remove unnessesary ids

--- server/data/seeds/001_seed_wishlist.js
+++ server/data/seeds/001_seed_wishlist.js
@@ -3,12 +3,12 @@ exports.seed = function (knex) {
     .del()
     .then(() => {
       return knex('wishlist').insert([
-        { id: 1, name: "Bob's wishlist", author: 'Bob' },
-        { id: 2, name: "Allens's wishlist", author: 'Allen' },
-        { id: 3, name: "Gavin's wishlist", author: 'Gavin' },
-        { id: 4, name: "Tim's wishlist", author: 'Tim' },
-        { id: 5, name: "Dave's wishlist", author: 'Dave' },
-        { id: 6, name: "Sarah's wishlist", author: 'Sarah' },
+        { name: "Bob's wishlist", author: 'Bob' },
+        { name: "Allens's wishlist", author: 'Allen' },
+        { name: "Gavin's wishlist", author: 'Gavin' },
+        { name: "Tim's wishlist", author: 'Tim' },
+        { name: "Dave's wishlist", author: 'Dave' },
+        { name: "Sarah's wishlist", author: 'Sarah' },
       ]);
     });
 };
--- server/data/seeds/002_seed_item.js
+++ server/data/seeds/002_seed_item.js
@@ -3,21 +3,21 @@ exports.seed = function (knex) {
     .del()
     .then(() => {
       return knex('item').insert([
-        { id: 1, wishlist_id: 1, name: 'skateboard' },
-        { id: 2, wishlist_id: 1, name: 'toothbrush' },
-        { id: 3, wishlist_id: 2, name: 'coal' },
-        { id: 4, wishlist_id: 3, name: 'gavel' },
-        { id: 5, wishlist_id: 3, name: '4K TV' },
-        { id: 6, wishlist_id: 3, name: 'Vive' },
-        { id: 7, wishlist_id: 4, name: 'People to stop calling him Tim-Tim' },
-        { id: 8, wishlist_id: 4, name: 'tissue box' },
-        { id: 9, wishlist_id: 4, name: 'plate' },
-        { id: 10, wishlist_id: 5, name: 'remote' },
-        { id: 11, wishlist_id: 5, name: 'pair of socks' },
-        { id: 12, wishlist_id: 5, name: 'key' },
-        { id: 13, wishlist_id: 6, name: 'coffee mug' },
-        { id: 14, wishlist_id: 6, name: 'tennis ball' },
-        { id: 15, wishlist_id: 6, name: 'tiger' },
+        { wishlist_id: 1, name: 'skateboard' },
+        { wishlist_id: 1, name: 'toothbrush' },
+        { wishlist_id: 2, name: 'coal' },
+        { wishlist_id: 3, name: 'gavel' },
+        { wishlist_id: 3, name: '4K TV' },
+        { wishlist_id: 3, name: 'Vive' },
+        { wishlist_id: 4, name: 'People to stop calling him Tim-Tim' },
+        { wishlist_id: 4, name: 'tissue box' },
+        { wishlist_id: 4, name: 'plate' },
+        { wishlist_id: 5, name: 'remote' },
+        { wishlist_id: 5, name: 'pair of socks' },
+        { wishlist_id: 5, name: 'key' },
+        { wishlist_id: 6, name: 'coffee mug' },
+        { wishlist_id: 6, name: 'tennis ball' },
+        { wishlist_id: 6, name: 'tiger' },
       ]);
     });
 };
