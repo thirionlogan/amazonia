c28f37bcad7ca4ae5cc39179fe27bd22c13aa062
SippingTrix <39608585+SippingTrix@users.noreply.github.com>
2020-12-18 14:25:38 -0700

    added tests for state and rendering the homepage
    Co-authored-by: Brent Ogunmakin <blackychan4234@gmail.com>

---
+++ src/components/Home/Home.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import PageHeader from '../PageHeader/PageHeader'
+
+
+
+class Home extends React.Component{
+    state = {
+        test: 'test'
+    }
+
+render(){
+    return(
+        <PageHeader/>
+    )
+}
+}
+  
+
+
+
+
+
+
+
+
+export default Home
