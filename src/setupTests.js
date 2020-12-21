de1dd51a8505d7f7f60efc79ae907300adf57ff4
Logan Thirion <thirionlogan@gmail.com>
2020-12-21 15:32:29 -0600

    implement WihslistEditor

--- src/setupTests.js
+++ src/setupTests.js
@@ -5,6 +5,6 @@
 import '@testing-library/jest-dom';
 
 import { configure } from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
+import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
 
-configure({adapter: new Adapter() })
+configure({ adapter: new Adapter() });
