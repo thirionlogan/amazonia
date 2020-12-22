1c45889eb834d29ebebe9020321c7eda1dc99a8d
Logan Thirion <thirionlogan@gmail.com>
2020-12-22 14:24:35 -0600

    add async testing

--- src/components/HomePage/HomePage.test.js
+++ src/components/HomePage/HomePage.test.js
@@ -4,20 +4,25 @@ import { handleGetWishlists } from '../../client/client';
 import HomePage from './HomePage';
 import { Accordion } from '@material-ui/core/';
 
+const flushPromises = () => new Promise(setImmediate);
+
 describe('HomePage', () => {
   let component;
 
   const mockHandleGetWishlists = jest.fn();
 
-  beforeEach(() => {
+  beforeEach(async () => {
     mockHandleGetWishlists.mockImplementation(handleGetWishlists);
     component = mount(<HomePage handleGetWishlists={mockHandleGetWishlists} />);
+    await flushPromises();
+    component.update();
   });
 
   afterEach(() => {
     mockHandleGetWishlists.mockReset();
   });
-  it('should load wishlists', () => {
+
+  it('should load wishlists', async () => {
     expect(mockHandleGetWishlists).toBeCalled();
     expect(component.find(Accordion)).toHaveLength(2);
   });
