d4564ebe25d8c14fb583a4d3d473b8093d8a7b0c
Logan Thirion <thirionlogan@gmail.com>
2020-12-22 12:08:53 -0600

    add mock service worker
    Co-authored-by: Brent Ogunmakin <blackychan4234@gmail.com>

--- src/setupTests.js
+++ src/setupTests.js
@@ -6,5 +6,21 @@ import '@testing-library/jest-dom';
 
 import { configure } from 'enzyme';
 import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
+import { server } from './mocks/server';
 
 configure({ adapter: new Adapter() });
+
+beforeAll(() => {
+  // Enable the mocking in tests.
+  server.listen();
+});
+
+afterEach(() => {
+  // Reset any runtime handlers tests may use.
+  server.resetHandlers();
+});
+
+afterAll(() => {
+  // Clean up once the tests are done.
+  server.close();
+});
